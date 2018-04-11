d4a920a8305bbb36fa4a930f48d9b7786a0267b0
gpeleritti <gpeleritti@sparkdigital.com>
2018-04-10 21:24:14 -0300

    addrelated link url control

--- avonCTAPlugin.js
+++ avonCTAPlugin.js
@@ -3,12 +3,13 @@ videojs.registerPlugin('avonCTAPlugin', function() {
     player.ready(function() {
         player.on('loadedmetadata',function(){
             var info = player.mediainfo;
-            console.log('Info: ', info);
-            var buttonCTA = document.createElement('a');
-            buttonCTA.setAttribute('class', 'cta-button');
-            buttonCTA.setAttribute('href', info.relatedLink);
-            buttonCTA.innerHTML = 'Buy Now!';
-            document.getElementsByClassName('video-js')[0].append(buttonCTA);
+            if (info.link && info.link.url) {
+                var buttonCTA = document.createElement('a');
+                buttonCTA.setAttribute('class', 'cta-button');
+                buttonCTA.setAttribute('href', info.link.url);
+                buttonCTA.innerHTML = 'Buy Now!';
+                document.getElementsByClassName('video-js')[0].append(buttonCTA);
+            }
         });
     });
 
