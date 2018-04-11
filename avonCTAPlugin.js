f82c304ffaf2df377eccc3bbd81d269467047d72
gpeleritti <gpeleritti@sparkdigital.com>
2018-04-11 18:31:25 -0300

    adding text and url for testing

--- avonCTAPlugin.js
+++ avonCTAPlugin.js
@@ -3,11 +3,15 @@ videojs.registerPlugin('avonCTAPlugin', function() {
     player.ready(function() {
         player.on('loadedmetadata',function(){
             var info = player.mediainfo;
+            info.link = info.link? info.link : {
+                url: 'http://www.google.com'
+            }
             if (info.link && info.link.url) {
                 var buttonCTA = document.createElement('a');
                 buttonCTA.setAttribute('class', 'cta-button');
                 buttonCTA.setAttribute('href', info.link.url);
-                buttonCTA.innerHTML = 'Buy Now!';
+                buttonCTA.setAttribute('target', '_blank');
+                buttonCTA.innerHTML = info.link.text? info.link.text : 'Buy Now!';
                 document.getElementsByClassName('video-js')[0].append(buttonCTA);
             }
         });
