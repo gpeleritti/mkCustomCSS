cef5a5efaed11316306cc86f60a874793c68255a
gpeleritti <gpeleritti@sparkdigital.com>
2018-04-13 10:57:06 -0300

    adding baseUrl option

--- avonPlugin.js
+++ avonPlugin.js
@@ -17,7 +17,9 @@ videojs.registerPlugin('avonSharePlugin', function() {
             var rep = window.location.search.split('repid=')[1];
             var repid = rep? rep.split('&')[0] : 'undefined';
 
-            var url = 'https://www.avon.com/video-share?video='+info.id+'&player='+player.bcinfo.playerId +'&repid='+repid;
+            console.log('BASEURL: ', options.baseUrl);
+
+            var url = options.baseUrl+'?video='+info.id+'&player='+player.bcinfo.playerId +'&repid='+repid;
             socialOverlay.setDirectLink(url);
         });
     });
