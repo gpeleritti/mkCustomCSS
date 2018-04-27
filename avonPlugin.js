af112f5b7049f2ad7ee169b53c3ef6a9a80d06cd
gpeleritti <gpeleritti@sparkdigital.com>
2018-04-27 12:23:34 -0300

    adding loadedmetadata event and structuring code

--- avonPlugin.js
+++ avonPlugin.js
@@ -1,6 +1,38 @@
 videojs.registerPlugin('avonSharePlugin', function(options) {
     var player = this;
 
+    function getDataForPlayer(player) {
+        var info = player.mediainfo,
+            socialOverlay = player.socialOverlay;
+        if (info.tags && info.tags.indexOf('shareable') > -1){
+            player.socialButton.show();
+        } else {
+            player.socialButton.hide();
+        }
+        var rep = window.location.search.split('repid=')[1];
+        var repid = rep? rep.split('&')[0] : 'undefined';
+
+        //use customURL as base url if is present if not use the defgault
+        var urlbase = options.customURL? options.customURL : 'https://www.avon.com/video-share';
+
+        //Use the value of player id passed on the plugin options. If not use the current player Id.
+        var playerId = options.playerId? options.playerId : player.bcinfo.playerId;
+
+        //query parameters
+        var qp = '?video='+info.id+'&player='+playerId+'&repid='+repid;
+
+        // replace the query parameters on the embed code
+        var ec = socialOverlay.getEmbedCode();
+        var qsStart = ec.indexOf('?');
+        var qsEnd = ec.split('?')[1].indexOf(' ') + qsStart;
+        var finalEC = ec.replace(ec.substring(qsStart, qsEnd), qp);
+
+        var url = urlbase + qp;
+
+        socialOverlay.setDirectLink(url);
+        socialOverlay.setEmbedCode(finalEC);
+    };
+
     player.ready(function() {
 
         if (player && player.socialButton) {
@@ -8,67 +40,11 @@ videojs.registerPlugin('avonSharePlugin', function(options) {
         }
 
         player.on('loadstart',function(){
-            var info = player.mediainfo,
-                socialOverlay = player.socialOverlay;
-            if (info.tags && info.tags.indexOf('shareable') > -1){
-                player.socialButton.show();
-            } else {
-                player.socialButton.hide();
-            }
-            var rep = window.location.search.split('repid=')[1];
-            var repid = rep? rep.split('&')[0] : 'undefined';
-
-            //use customURL as base url if is present if not use the defgault
-            var urlbase = options.customURL? options.customURL : 'https://www.avon.com/video-share';
-
-            //Use the value of player id passed on the plugin options. If not use the current player Id.
-            var playerId = options.playerId? options.playerId : player.bcinfo.playerId;
-
-            //query parameters
-            var qp = '?video='+info.id+'&player='+playerId+'&repid='+repid;
-
-            // replace the query parameters on the embed code
-            var ec = socialOverlay.getEmbedCode();
-            var qsStart = ec.indexOf('?');
-            var qsEnd = ec.split('?')[1].indexOf(' ') + qsStart;
-            var finalEC = ec.replace(ec.substring(qsStart, qsEnd), qp);
-
-            var url = urlbase + qp;
-
-            socialOverlay.setDirectLink(url);
-            socialOverlay.setEmbedCode(finalEC);
+            getDataForPlayer(player);
         });
 
         player.on('loadedmetadata',function(){
-            var info = player.mediainfo,
-                socialOverlay = player.socialOverlay;
-            if (info.tags && info.tags.indexOf('shareable') > -1){
-                player.socialButton.show();
-            } else {
-                player.socialButton.hide();
-            }
-            var rep = window.location.search.split('repid=')[1];
-            var repid = rep? rep.split('&')[0] : 'undefined';
-
-            //use customURL as base url if is present if not use the defgault
-            var urlbase = options.customURL? options.customURL : 'https://www.avon.com/video-share';
-
-            //Use the value of player id passed on the plugin options. If not use the current player Id.
-            var playerId = options.playerId? options.playerId : player.bcinfo.playerId;
-
-            //query parameters
-            var qp = '?video='+info.id+'&player='+playerId+'&repid='+repid;
-
-            // replace the query parameters on the embed code
-            var ec = socialOverlay.getEmbedCode();
-            var qsStart = ec.indexOf('?');
-            var qsEnd = ec.split('?')[1].indexOf(' ') + qsStart;
-            var finalEC = ec.replace(ec.substring(qsStart, qsEnd), qp);
-
-            var url = urlbase + qp;
-
-            socialOverlay.setDirectLink(url);
-            socialOverlay.setEmbedCode(finalEC);
+            getDataForPlayer(player);
         });
 
         player.on('endscreen', function(event){
